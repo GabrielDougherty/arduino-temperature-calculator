0f0767cdfd4afc3fdfa2c80245a86c2109fc0287
Gabriel Dougherty <contact@gabrieldougherty.com>
2023-09-02 15:57:06 -0400

    All working now

---
+++ README.md
@@ -0,0 +1,9 @@
+# Arduino temperature converter project
+
+This is an arduino project to convert Fahrenheit temperatures (entered on a keypad) to Celsius. The converted temperature is displayed as soon as the second digit of the temperature is entered. To convert a single digit temperature, hit the # or * key after the single digit number.
+
+![A demo of the project working](static/demo.gif)
+
+# Why
+
+Mainly because I wanted to learn Arduino programming. But also because at my job, I work with folks outside of the US who want to know what the weather has been like recently. I wanted a quick way to convert a temperature in my head (like I know it has been 88F recently) into a Celsius temperature.
--- sketch_aug26a.ino
+++ sketch_aug26a.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
+
 const int ROW_NUM = 4; //four rows
 const int COLUMN_NUM = 3; //three columns
 
@@ -17,27 +18,6 @@ byte pin_column[COLUMN_NUM] = {0, 1, 2}; //connect to the column pinouts of the 
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 
-void printToLcd(const char *str)
-{
-  byte error;
-  Wire.begin();
-  Wire.beginTransmission(0x27);
-  error = Wire.endTransmission();
-  if(error == 0) {                      // If your device is 0x27
-    LiquidCrystal_I2C lcd(0x27,20,4);
-    lcd.init(); 
-    lcd.backlight();
-    lcd.setCursor(0,0);
-    lcd.print(str);
-  } else {                               // If your device is 0x3F
-    LiquidCrystal_I2C lcd(0x3F,20,4);
-    lcd.init(); 
-    lcd.backlight();
-    lcd.setCursor(0,0);
-    lcd.print(str);
-  }
-}
-
 class WirePrinter
 {
   public:
@@ -49,6 +29,11 @@ class WirePrinter
   {
     Wire.begin();
     Wire.beginTransmission(0x27);
+    byte error;
+    error = Wire.endTransmission();
+    if(error != 0) {                               // If your device is 0x3F
+      lcd = LiquidCrystal_I2C(0x3F,20,4);
+    }
     lcd.init(); 
     lcd.backlight();
     lcd.setCursor(0,0);
@@ -72,15 +57,37 @@ void setup()
   printer.print("Enter a key");
 }
 
-char buf[10];
+int i = 0;
+char buf[20];
+char buf2[20];
+
 void loop()
 {
   char key = keypad.getKey();
 
   if (key)
   {
-    buf[0] = key;
-    buf[1] = '\0';
-    printer.print(buf);
+    buf[i] = key;
+    buf[i+1] = '\0';
+    if (i == 1)
+    {
+      char * pEnd;
+      int tmpNum = strtol(buf, &pEnd, 10);
+      if (pEnd != buf)
+      {
+        int celsius = 5.0/9.0 * static_cast<double>(tmpNum-32) + 0.5;
+        itoa(celsius, buf2, 10);
+        strcat(buf, "F = ");
+        strcat(buf, buf2);
+        strcat(buf, "C.");
+        printer.print(buf);
+        buf[0] = '\0';
+        buf2[0] = '\0';
+      }
+    } else
+    {
+      printer.print(buf);
+    }
+    i = (i + 1) % 2;
   }
 }
